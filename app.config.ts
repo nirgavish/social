023bbb6dbb4e9eb9b8b5d90bbc619c8812ff06a6
Nir Gavish <nirg@razorgrip.com>
2018-08-11 01:34:08 +0300

    Updated app.config

--- app.config.ts
+++ app.config.ts
@@ -1,18 +1,27 @@
 export const Config = {
 
     /*
-    A walled garden is a community that only allows admins to register new users
+    Content is available only to logged-in users
     */
+    // TODO: Implement walledGarden
     walledGarden: false,
+
+    /*
+    When set to false, only admins can add new users
+    */
+    // TODO: Implement usersMayRegister
+    usersMayRegister: true,
 
     /*
     if no S3 credentials are present, local file system is used
     */
+    // TODO: Implement S3 and filesystem
     S3Credentials: {},
 
     /*
     When set to false, only admins can start a group
     */
+    // TODO: Implement usersMayStartGroups
     usersMayStartGroups: true,
 
     /*
