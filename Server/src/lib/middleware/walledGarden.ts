d5742ff364698283ab708ced69c6993c9035d630
Nir Gavish <nirg@razorgrip.com>
2018-08-12 01:31:29 +0300

    walledGarden middleware

--- Server/src/lib/middleware/walledGarden.ts
+++ Server/src/lib/middleware/walledGarden.ts
@@ -2,6 +2,17 @@ import {Request, Response} from "express";
 import {Config} from "../../../../app.config";
 
 export function WalledGarden(req: Request, res: Response, next) {
-    // TODO: Implement walled garden middleware
-    next();
+    // TODO: Can't logout when walledGarden set to true
+
+    if (Config.walledGarden && !req.user.isLoggedIn) {
+        if(req.originalUrl !== "/auth/login") {
+            res.status(401).end();
+            next(new Error("Unauthenticated user!"));
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+
 }
