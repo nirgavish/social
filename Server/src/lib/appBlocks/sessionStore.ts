be030a82946e9cfc6ced9c81a8788395976f2f54
Nir Gavish <nirg@razorgrip.com>
2018-08-16 12:01:36 +0300

    Moved session secret hash to .env

--- Server/src/lib/appBlocks/sessionStore.ts
+++ Server/src/lib/appBlocks/sessionStore.ts
@@ -5,11 +5,11 @@ const FileStore = sessionFilestore(session);
 export const SessionStore = session({
     store: new FileStore({
         path: "./sessions",
-        secret: "}6lSh,V}_*O|ZOip2X@\"Focdd\">Ga~Yc0*wbHU$9cuMf0A>W[u?3@A=zK_:rEv*",
+        secret: process.env.session_secret,
         logFn: () => {},
         ttl: 3600000000000,
     }),
-    secret: "}6lSh,V}_*O|ZOip2X@\"Focdd\">Ga~Yc0*wbHU$9cuMf0A>W[u?3@A=zK_:rEv*",
+    secret: process.env.session_secret,
     resave: false,
     saveUninitialized: false,
 })
